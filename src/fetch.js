1c5266916eadf4f0fb39c18dafc25a0077c63b37
Lockett Pundt <kalifornium@hotmail.com>
2020-03-24 14:35:58 -0400

    exported the fetch function for categories.

--- src/fetch.js
+++ src/fetch.js
@@ -5,27 +5,30 @@ const submitButton = document.getElementById("submitButton");
 let chuckSays = document.querySelector(".chuckSays");
 const closeModalButton = document.querySelector("#closeModal");
 
-const getCategory = async () => {
+export const getCategory = async () => {
   try {
     const response = await fetch(`https://api.chucknorris.io/jokes/categories`);
     const data = await response.json();
+    let arr = [];
+    data.map(item => arr.push(item));
+    return arr;
     // console.log(data.filter(item => item !== "explicit"));
-    const categoryDropdown = document.createElement("select");
-    const categoryList = data.filter(item => item !== "explicit");
-    categoryList.map(listItem => {
-      const selectItem = document.createElement("option");
-      selectItem.value = listItem;
-      selectItem.text = listItem;
-      categoryDropdown.appendChild(selectItem);
-      return categoryList;
-    });
-    formLocation.insertBefore(categoryDropdown, submitButton);
+    // const categoryDropdown = document.createElement("select");
+    // const categoryList = data.filter(item => item !== "explicit");
+    // categoryList.map(listItem => {
+    //   const selectItem = document.createElement("option");
+    //   selectItem.value = listItem;
+    //   selectItem.text = listItem;
+    //   categoryDropdown.appendChild(selectItem);
+    //   return categoryList;
+    // });
+    // formLocation.insertBefore(categoryDropdown, submitButton);
   } catch (error) {
     return error;
   }
 };
 
-getCategory();
+// getCategory();
 
 //   const categoryList = response.filter(item => item !== `explicit`);
 //   categoryList.map(listItem => {
