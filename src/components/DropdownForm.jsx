aedc74cf377826d76df20ad6cdbe0d79ab11979c
Lockett Pundt <kalifornium@hotmail.com>
2020-03-24 14:34:05 -0400

    trying to get a fetch to get categories.

--- src/components/DropdownForm.jsx
+++ src/components/DropdownForm.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 
-import "../fetch.js";
+import * as fetch from "../fetch.js";
 
 class Dropdown extends Component {
+  
   render() {
+    const categories = fetch.getCategory();
+    console.log(categories);
+    
     return (
       <form className="dropdown" id="categorySelect">
         <button className="buttonStyle" type="submit" id="submitButton">
