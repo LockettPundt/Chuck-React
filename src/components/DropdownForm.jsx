2305d38bf0bc530918d168d12075630869128498
Lockett Pundt <kalifornium@hotmail.com>
2020-03-24 15:53:59 -0400

    got fetch working with the componentmount function. then it populates the dropdown menu with the array of option. (sans explicit).

--- src/components/DropdownForm.jsx
+++ src/components/DropdownForm.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
 
-import * as fetch from "../fetch.js";
+// import * as fetch from "../fetch.js";
 
 class Dropdown extends Component {
   
+  state = {
+    data: [],
+  }
+  
+  async getCategory() {
+      const response = await fetch(`https://api.chucknorris.io/jokes/categories`);
+      const json = await response.json();
+      const result = json.filter(item => item !== "explicit")
+      this.setState({ data: result});
+  }
+  componentDidMount() {
+    this.getCategory();
+  }
+  
+  
   render() {
-    const categories = fetch.getCategory();
-    console.log(categories);
+    // console.log("this is the data", this.state.data);
     
     return (
       <form className="dropdown" id="categorySelect">
+        <select className="categorySelect">
+          {
+          this.state.data.map((item) =>  <option name={item} value ={item}>{item}</option>
+          )}
+        </select>
         <button className="buttonStyle" type="submit" id="submitButton">
           SUBMIT!
         </button>
