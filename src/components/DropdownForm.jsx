3e74fa43ea7b74c11f9f542abb60f9f2b153f7be
Lockett Pundt <kalifornium@hotmail.com>
2020-03-25 14:12:18 -0400

    fetch categories works to fill ou t the drop down.

--- src/components/DropdownForm.jsx
+++ src/components/DropdownForm.jsx
@@ -3,36 +3,60 @@ import React, { Component } from "react";
 // import * as fetch from "../fetch.js";
 
 class Dropdown extends Component {
-  
-  state = {
-    data: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      selectedOption: '',
+    };
   }
   
-  async getCategory() {
+  
+  getCategory = async () => {
       const response = await fetch(`https://api.chucknorris.io/jokes/categories`);
       const json = await response.json();
       const result = json.filter(item => item !== "explicit")
       this.setState({ data: result});
   }
+  getQuote = async () => {
+    const response = await fetch('https://api.chucknorris.io/jokes/random?category=animal');
+    const json = await response.json();
+    console.log("THis is the quote:", json.value);
+  }
+  
   componentDidMount() {
     this.getCategory();
+  
   }
   
   
+  
+  
+  
+  
   render() {
-    // console.log("this is the data", this.state.data);
-    
+   // let modalStatus;
     return (
-      <form className="dropdown" id="categorySelect">
-        <select className="categorySelect">
-          {
-          this.state.data.map((item) =>  <option name={item} value ={item}>{item}</option>
-          )}
-        </select>
-        <button className="buttonStyle" type="submit" id="submitButton">
-          SUBMIT!
-        </button>
-      </form>
+      <div>
+        <form className="dropdown" id="categorySelect">
+          <select className="categorySelect">
+            {
+            this.state.data.map((item) =>  <option value={item}>{item}</option>
+            )}
+          </select>
+          <button className="buttonStyle" type="submit" id="submitButton" onClick={this.getQuote}>
+            SUBMIT!
+          </button>
+        </form>
+        <div className="modal-overlay">
+        <div className="modal-content">
+          <span id="closeModal" className="close-modal">
+            X
+          </span>
+          <p className="chuckSays">...</p>
+        </div>
+      </div>
+      </div>
     );
   }
 }
