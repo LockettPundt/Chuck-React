5343910d3861cf27a506190bb1381a22e2b1abe7
Lockett Pundt <kalifornium@hotmail.com>
2020-03-26 15:20:14 -0400

    getting used to AirBnB styling.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './App.css';
-import Quote from "./components/Quote";
-import ChuckImage from "./components/ChuckImage";
-import DropdownForm from "./components/DropdownForm";
-import ChuckModal from "./components/ChuckModal";
+import Quote from './components/Quote';
+import ChuckImage from './components/ChuckImage';
+import DropdownForm from './components/DropdownForm';
+import ChuckModal from './components/ChuckModal';
 
 function App() {
   return (
     <div className="App">
-     <Quote />
+      <Quote />
       <ChuckImage />
       <DropdownForm />
       <ChuckModal />
