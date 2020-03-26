710159bb6806c2b9afae532d332a9f9f793e2da4
Lockett Pundt <kalifornium@hotmail.com>
2020-03-26 15:21:26 -0400

    destructered the state of the component. applies AirBnB styling.

--- src/components/DropdownForm.jsx
+++ src/components/DropdownForm.jsx
@@ -1,4 +1,7 @@
-import React, { Component } from "react";
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { Component } from 'react';
 
 // import * as fetch from "../fetch.js";
 
@@ -7,75 +10,66 @@ class Dropdown extends Component {
     super(props);
     this.state = {
       data: [],
-      selectedOption: '',
-      category: "animal",
+      category: 'animal',
       quote: '',
     };
   }
-  
-  
-  getCategory = async () => {
-    const response = await fetch(`https://api.chucknorris.io/jokes/categories`);
-    const json = await response.json();
-    const result = json.filter(item => item !== "explicit")
-    this.setState({ data: result});
+
+
+  componentDidMount() {
+    this.getCategory();
   }
-  
+
+  getCategory = async () => {
+    const response = await fetch('https://api.chucknorris.io/jokes/categories');
+    const json = await response.json();
+    const result = json.filter((item) => item !== 'explicit');
+    this.setState({ data: result });
+  }
+
+  newCategory = () => {
+    const newValue = document.querySelector('select').value;
+    this.setState({
+      category: newValue,
+    });
+  }
+
   getQuote = async (e) => {
     e.preventDefault();
     const { category } = this.state;
     const response = await fetch(`https://api.chucknorris.io/jokes/random?category=${category}`);
     const json = await response.json();
-    let newQuote = json.value;
+    const newQuote = json.value;
     const modalWindow = document.querySelector('.modal-overlay');
     modalWindow.classList.toggle('open');
     this.setState({
       quote: newQuote,
-    })
+    });
   }
-  
-  componentDidMount() {
-    this.getCategory();
-  
-  }
-  
-  newCategory = () => {
-    const newValue = document.querySelector('select').value;
-    this.setState({
-      category: newValue,
-    })
-  }
-  
+
   closeModalButton = (e) => {
     e.preventDefault();
     const modalWindow = document.querySelector('.modal-overlay');
     modalWindow.classList.toggle('open');
   };
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
   render() {
-    const { quote } = this.state;
+    const { quote, data } = this.state;
     return (
       <div>
         <form className="dropdown" id="categorySelect">
           <select className="categorySelect" onChange={this.newCategory}>
             {
-            this.state.data.map((item) =>  <option value={item}>{item}</option>
-            )}
+            data.map((item, index) => <option value={item} key={index}>{item}</option>)
+}
           </select>
           <button className="buttonStyle" type="submit" id="submitButton" onClick={this.getQuote}>
             SUBMIT!
           </button>
         </form>
         <div className="modal-overlay">
-          
+
           <div className="modal-content">
             <span id="closeModal" className="close-modal" onClick={this.closeModalButton}>
               X
